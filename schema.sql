24aefe3a2ff3cb49d3a47f3b5f51a34be395ebad
kintaro~ <hbakumeayato@gmail.com~>
2026-05-14 14:22:56 +0900

    hrenkou

--- schema.sql
+++ schema.sql
@@ -1,2 +1,10 @@
---usersコード
-CREATE TABLE ;;gh
+-- profiles テーブルを作成するコード
+CREATE TABLE profiles (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL UNIQUE,
+    display_name VARCHAR(50),
+    bio TEXT,
+    birth_date DATE,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES password(user_id)
+);
